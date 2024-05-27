deaefd6715a145dd0e624835fdefb94201071e4a
Vitor Marinho <vitor@uw-s.nl>
2024-05-27 23:06:29 +0200

    about page added

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>About Page</h1>
+        </main>
+    )
+}
+
+export default About;
