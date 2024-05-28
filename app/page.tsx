6a8c367923ec5352775cfe29674377d813af6b6f
Vitor Marinho <vitor@uw-s.nl>
2024-05-28 23:52:21 +0200

    flattenAttributes function added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,46 @@
-import { Button } from "@/components/ui/button";
+import qs from "qs";
+import { flattenAttributes } from "@/lib/utils";
+import { HeroSection } from "@/components/custom/HeroSection";
 
-export default function Home() {
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      populate: {
+        image: {
+          fields: ["url", "alternativeText"],
+        },
+        link: {
+          populate: true,
+        },
+      },
+    },
+  },
+});
+
+async function getStrapiData(path: string) {
+  const baseUrl = "http://localhost:1337";
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
+  try {
+    const response = await fetch(url.href);
+    const data = await response.json();
+    const flattenedData = flattenAttributes(data);
+    return flattenedData;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
+  // console.dir(strapiData, { depth: null });
+
+  const { blocks } = await strapiData;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <h1>
-            Home page
-          </h1>
-          <Button>A button</Button>
+    <main>
+      <HeroSection data={blocks[0]} />
     </main>
   );
 }
