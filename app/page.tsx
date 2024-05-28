cef551b499fd60026088f8a278238ac9d6482ad0
Vitor Marinho <vitor@uw-s.nl>
2024-05-28 12:16:49 +0200

    added radix-ui peer dependencies eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -6,6 +6,7 @@ export default function Home() {
           <h1>
             Home page
           </h1>
+          <Button>A button</Button>
     </main>
   );
 }
