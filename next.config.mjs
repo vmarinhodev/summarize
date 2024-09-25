a09789d5247afe31180c39a9a425c6e482896b03
Vitor Marinho <vitor@uw-s.nl>
2024-09-25 12:09:12 +0200

    libstrapi changed

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
             },
             {
                 protocol: "https",
-                hostname: "placehold.co",
+                hostname: "https://summarize-strapi.onrender.com/",
             },
         ],
     },
